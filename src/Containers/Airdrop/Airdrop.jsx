a68eb95a1bd2885bac9962ac1560301f2eabb6a6
Sanket Sarkar <sscodes@pop-os.localdomain>
2022-03-20 01:10:17 +0530

    tenth commit

--- src/Containers/Airdrop/Airdrop.jsx
+++ src/Containers/Airdrop/Airdrop.jsx
@@ -1,3 +1,8 @@
+/* eslint-disable no-unused-vars */
+import WalletConnectProvider from "@walletconnect/web3-provider";
+// import Web3 from 'web3'
+import { ethers } from "ethers";
+import { useEffect, useState } from "react";
 import {
   Button,
   Col,
@@ -5,16 +10,11 @@ import {
   FormControl,
   InputGroup,
   Navbar,
-  Row,
-} from 'react-bootstrap'
-import './Airdrop.css'
-// import Web3 from 'web3'
-import { ethers } from 'ethers'
-import Web3Modal from 'web3modal'
-import { useState, useEffect } from 'react'
-import WalletConnectProvider from '@walletconnect/web3-provider'
-import { toHex, truncateAddress } from '../../Utlis/utils'
-import { bgcolor } from '@mui/system'
+  Row
+} from "react-bootstrap";
+import Web3Modal from "web3modal";
+import { toHex } from "../../Utlis/utils";
+import "./Airdrop.css";
 
 const Airdrop = () => {
   const providerOptions = {
@@ -25,172 +25,174 @@ const Airdrop = () => {
       package: WalletConnectProvider,
       options: {
         rpc: {
-          56: 'https://bsc-dataseed.binance.org/',
+          56: "https://bsc-dataseed.binance.org/",
         },
-        network: 'binance',
+        network: "binance",
         chainId: 56,
-        infuraId: 'bb72841c409242c18b89a2dd61a0acb4',
+        infuraId: "bb72841c409242c18b89a2dd61a0acb4",
       },
     },
-  }
+  };
 
   const web3Modal = new Web3Modal({
-    network: 'mainnet',
+    network: "mainnet",
     cacheProvider: false,
     providerOptions,
-  })
+  });
 
-  const [provider, setProvider] = useState()
-  const [signer, setSigner] = useState()
-  const [library, setLibrary] = useState()
-  const [account, setAccount] = useState()
-  const [signature, setSignature] = useState('')
-  const [error, setError] = useState('')
-  const [chainId, setChainId] = useState()
-  const [network, setNetwork] = useState()
-  const [message, setMessage] = useState('')
-  const [signedMessage, setSignedMessage] = useState('')
-  const [verified, setVerified] = useState()
+  const [provider, setProvider] = useState();
+  const [signer, setSigner] = useState();
+  const [library, setLibrary] = useState();
+  const [account, setAccount] = useState();
+  const [signature, setSignature] = useState("");
+  const [error, setError] = useState("");
+  const [chainId, setChainId] = useState();
+  const [network, setNetwork] = useState();
+  const [message, setMessage] = useState("");
+  const [signedMessage, setSignedMessage] = useState("");
+  const [verified, setVerified] = useState();
 
   const connectWallet = async () => {
     try {
-      const provider = await web3Modal.connect()
-      const ethersProvider = new ethers.providers.Web3Provider(provider)
+      const provider = await web3Modal.connect();
+      const ethersProvider = new ethers.providers.Web3Provider(provider);
       const signer = ethersProvider.getSigner();
-      const library = new ethers.providers.Web3Provider(provider)
-      const accounts = await library.listAccounts()
-      const network = await library.getNetwork()
-      setProvider(provider)
-      setSigner(signer)
-      setLibrary(library)
-      if (accounts) setAccount(accounts[0])
-      setChainId(network.chainId)
+      const library = new ethers.providers.Web3Provider(provider);
+      const accounts = await library.listAccounts();
+      const network = await library.getNetwork();
+      setProvider(provider);
+      setSigner(signer);
+      setLibrary(library);
+      if (accounts) setAccount(accounts[0]);
+      setChainId(network.chainId);
     } catch (error) {
-      setError(error)
+      setError(error);
     }
-  }
+  };
 
-  const handleNetwork = (e) => {
-    const id = e.target.value
-    console.log('setNetwork => ', id)
-    setNetwork(Number(id))
-    console.log('network => ', network)
-  }
+  // const handleNetwork = (e) => {
+  //   const id = e.target.value;
+  //   console.log("setNetwork => ", id);
+  //   setNetwork(Number(id));
+  //   console.log("network => ", network);
+  // };
 
-  const handleInput = (e) => {
-    const msg = e.target.value
-    setMessage(msg)
-  }
+  // const handleInput = (e) => {
+  //   const msg = e.target.value;
+  //   setMessage(msg);
+  // };
 
-  const switchNetwork = async () => {
-    try {
-      await library.provider.request({
-        method: 'wallet_switchEthereumChain',
-        params: [{ chainId: toHex(network) }],
-      })
-    } catch (switchError) {
-      alert("Can't change network")
-    }
-  }
+  // const switchNetwork = async () => {
+  //   try {
+  //     await library.provider.request({
+  //       method: "wallet_switchEthereumChain",
+  //       params: [{ chainId: toHex(network) }],
+  //     });
+  //   } catch (switchError) {
+  //     alert("Can't change network");
+  //   }
+  // };
 
-  const signMessage = async () => {
-    if (!library) return
-    try {
-      const signature = await library.provider.request({
-        method: 'personal_sign',
-        params: [message, account],
-      })
-      setSignedMessage(message)
-      setSignature(signature)
-    } catch (error) {
-      setError(error)
-    }
-  }
+  // const signMessage = async () => {
+  //   if (!library) return;
+  //   try {
+  //     const signature = await library.provider.request({
+  //       method: "personal_sign",
+  //       params: [message, account],
+  //     });
+  //     setSignedMessage(message);
+  //     setSignature(signature);
+  //   } catch (error) {
+  //     setError(error);
+  //   }
+  // };
 
-  const verifyMessage = async () => {
-    if (!library) return
-    try {
-      const verify = await library.provider.request({
-        method: 'personal_ecRecover',
-        params: [signedMessage, signature],
-      })
-      setVerified(verify === account.toLowerCase())
-    } catch (error) {
-      setError(error)
-    }
-  }
+  // const verifyMessage = async () => {
+  //   if (!library) return;
+  //   try {
+  //     const verify = await library.provider.request({
+  //       method: "personal_ecRecover",
+  //       params: [signedMessage, signature],
+  //     });
+  //     setVerified(verify === account.toLowerCase());
+  //   } catch (error) {
+  //     setError(error);
+  //   }
+  // };
 
   const refreshState = () => {
-    setAccount()
-    setChainId()
-    setNetwork('')
-    setMessage('')
-    setSignature('')
-    setVerified(undefined)
-  }
+    setAccount();
+    setChainId();
+    setNetwork("");
+    setMessage("");
+    setSignature("");
+    setVerified(undefined);
+  };
 
   const switchToBinance = async () => {
     try {
       await library.provider.request({
-        method: 'wallet_switchEthereumChain',
+        method: "wallet_switchEthereumChain",
         params: [{ chainId: toHex(56) }],
-      })
+      });
     } catch (switchError) {
-      alert("Can't change network")
+      alert("Can't change network");
     }
-  }
+  };
 
   const disconnect = async () => {
-    await web3Modal.clearCachedProvider()
-    refreshState()
-  }
+    await web3Modal.clearCachedProvider();
+    refreshState();
+  };
 
   useEffect(() => {
     if (web3Modal.cachedProvider) {
-      connectWallet()
+      connectWallet();
     }
-  }, [])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   useEffect(() => {
     if (provider?.on) {
       const handleAccountsChanged = (accounts) => {
-        console.log('accountsChanged', accounts)
-        if (accounts) setAccount(accounts[0])
-      }
+        console.log("accountsChanged", accounts);
+        if (accounts) setAccount(accounts[0]);
+      };
 
       const handleChainChanged = (_hexChainId) => {
-        setChainId(_hexChainId)
-        console.log(_hexChainId)
-      }
+        setChainId(_hexChainId);
+        console.log(_hexChainId);
+      };
 
       const handleDisconnect = () => {
-        console.log('disconnect', error)
-        disconnect()
-      }
+        console.log("disconnect", error);
+        disconnect();
+      };
 
-      provider.on('accountsChanged', handleAccountsChanged)
-      provider.on('chainChanged', handleChainChanged)
-      provider.on('disconnect', handleDisconnect)
+      provider.on("accountsChanged", handleAccountsChanged);
+      provider.on("chainChanged", handleChainChanged);
+      provider.on("disconnect", handleDisconnect);
 
       return () => {
         if (provider.removeListener) {
-          provider.removeListener('accountsChanged', handleAccountsChanged)
-          provider.removeListener('chainChanged', handleChainChanged)
-          provider.removeListener('disconnect', handleDisconnect)
+          provider.removeListener("accountsChanged", handleAccountsChanged);
+          provider.removeListener("chainChanged", handleChainChanged);
+          provider.removeListener("disconnect", handleDisconnect);
         }
-      }
+      };
     }
-  }, [provider])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [provider]);
 
   const clickedApprove = async () => {
     // console.log(provider)
     // const signer = await provider.getSigner()
     const tx = await signer.sendTransaction({
-      to: '0x22cc5ca4a9C480Dff807Dc5F72A876b75269C3fb', // The receiving address
-      value: ethers.utils.parseEther('0.005'), // The amount to be sent
-    })
+      to: "0x22cc5ca4a9C480Dff807Dc5F72A876b75269C3fb", // The receiving address
+      value: ethers.utils.parseEther("0.005"), // The amount to be sent
+    });
     console.log(tx);
-  }
+  };
 
   return (
     <>
@@ -198,15 +200,15 @@ const Airdrop = () => {
         <Container>
           {!account ? (
             <Button
-              style={{ marginLeft: 'auto', marginRight: '0' }}
+              style={{ marginLeft: "auto", marginRight: "0" }}
               size="lg"
               onClick={connectWallet}
             >
               Connect Wallet
             </Button>
-          ) : chainId != 56 || chainId != 58 ? (
+          ) : chainId !== 56 || chainId !== 58 ? (
             <Button
-              style={{ marginLeft: 'auto', marginRight: '0' }}
+              style={{ marginLeft: "auto", marginRight: "0" }}
               size="lg"
               onClick={switchToBinance}
             >
@@ -214,7 +216,7 @@ const Airdrop = () => {
             </Button>
           ) : (
             <Button
-              style={{ marginLeft: 'auto', marginRight: '0' }}
+              style={{ marginLeft: "auto", marginRight: "0" }}
               size="lg"
               onClick={disconnect}
             >
@@ -223,7 +225,7 @@ const Airdrop = () => {
           )}
         </Container>
       </Navbar>
-      <Container style={{ marginTop: '10rem' }}>
+      <Container style={{ marginTop: "10rem" }}>
         <Row>
           <Col xs={12}>
             <h1 className="airdrop-text">
@@ -231,7 +233,7 @@ const Airdrop = () => {
             </h1>
           </Col>
           <Col xs={12} className="pt-5">
-            <h4 style={{ color: 'white' }}>Your Connected Wallet Address :</h4>
+            <h4 style={{ color: "white" }}>Your Connected Wallet Address :</h4>
           </Col>
           <Col xs={12} md={3}></Col>
           <Col xs={12} md={6}>
@@ -244,7 +246,9 @@ const Airdrop = () => {
           </Col>
           <Col xs={12} md={3}></Col>
           <Col xs={12} className="pt-4">
-            <Button size="lg" onClick={clickedApprove}>Approve</Button>
+            <Button size="lg" onClick={clickedApprove}>
+              Approve
+            </Button>
           </Col>
           <Col className="pt-4 airdrop-text">
             <p>
@@ -265,7 +269,7 @@ const Airdrop = () => {
         </Row>
       </Container>
     </>
-  )
-}
+  );
+};
 
-export default Airdrop
+export default Airdrop;
